91012410e0243bdcb7185587e22181cf68db099a
tiregram <ruhtra.mar@gmail.com>
2015-11-19 18:36:16 +0100

    interface graphic

---
+++ src/DataCal.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
+
+class DataCal(pd.DataFrame):
+    
+    def BuilderDataCal(lis):
+        sh = dict()
+        for fe in lis:
+            if 'LOCATION'not in fe:
+                continue
+
+            keys = fe['LOCATION'].title()
+            #some event are place to many room
+            for key in keys.split(","):    
+                #TODO il ny a pas de check sur les evenement sur plusieur jour 
+                if key in sh:
+                    sh[key] = fe['DTEND'].dt.hour-fe['DTSTART'].dt.hour + sh[key]
+                else:
+                    sh[key] = fe['DTEND'].dt.hour-fe['DTSTART'].dt.hour
+
+        retDataFrame = DataCal(
+            {
+                "piece":list(sh.keys()),
+                "HD":list(sh.values())
+            })
+
+        retDataFrame = DataCal(retDataFrame.sort_values('HD'))
+        
+        return retDataFrame
+
+    def onlyRoom(self,  roomsToRemove):
+        roomList = roomsToRemove.split(',');
+        return DataCal(self.query('piece  in @roomList'))
+
+
+    def removeRoom(self , roomsToRemove):
+        roomList = roomsToRemove.split(',');
+        return self.query('piece not in @roomList')
+
+
+    def conditionRoom(self,exprre):
+        self['test'] = bb.piece.str.match(exprre)
+        return self.query('test == True')
+        #TODO remove test colums
+        
+    def render(self):
+        self.plot(kind='bar');
+        plt.ylabel('heure')
+        plt.xlabel('piece')
+        plt.title('Ocupation des pieces')
+        plt.xticks(range(0,len(self.piece)), self.piece ,rotation='vertical') 
+        plt.show()
+
--- src/main.py
+++ src/main.py
@@ -1,122 +1,37 @@
-import pycurl 
-import icalendar 
-from datetime import date 
+import Frame
 
-class DateDataForm:
-    resourceToGet = 0;
-    username="lecteur1"
-    password=""
-    dateStart   =  date(2015,9,3)
-    dateEnd     =  date(2016,5,15)
-    online=False
-    areDownload=False
-    areParse=False
-    icsFileName = "data/cal.ics"
-    optdict = None 
+   
 
-    def getDateToUrl(self):
-        ret = "startDay="+str(self.dateStart.day)
-        ret+= "&startMonth="+str(self.dateStart.month)
-        ret+= "&startYear="+str(self.dateStart.year)
-        ret+= "&endDay="+str(self.dateEnd.day)
-        ret+= "&endMonth="+str(self.dateEnd.month)
-        ret+= "&endYear="+str(self.dateEnd.year)
 
-        return ret
-        
+def getData(form):
+    import DataGet
+    lap = DataGet.DateDataForm()
+
+    lap.online      =not form.option_file.get()
+    lap.dateStart   = form.date1
+    lap.dateEnd     = form.date2
+    lap.icsFileName = form.file_path.get()
+
+    lis = lap.getDict('LOCATION','DESCRIPTION','DTSTART','DTEND')
     
-    def getICS(self):
-        if(self.online == False ):
-            return 
-
-
-
-        rooms="3474,682,683,684,685,772,2112,2841,719,2556,2842,183,185,196,4051,4679,749,737,154,713,700,1057,724,1858,705,2281,1908,758,707,708,759,712,714,2090,2108,725,726,701,715,716,163,167,2276,2272,2072,2074,2089,428,2274,2279,2265,717,720,721,722,745,704,746,747,748,3286,2781,2782,2117,728,2282,2270,2277,2278,2275,789,790,786,787,788,1852,780,4350,740,782,2584,742,741,731,732,734,736,735,674,998,727,733,680,659,665,2555,1295,5215,681,785,2743,5321,744,739,743,738,773,4937,775,776,147,3132,2907,2909,2911,2908,3134,2353,2844,2910,1135,2899,2912,2904,2905,2902,3129,2913,2898,2901,2007,3135,2903,1892,68,2273,2261,2703,2262,833,752,777,767,2031,779,1987,300,1861,1439,1357,2354,2573,90,686,1727,1578,65,590,1579,778"
- 
-        c = pycurl.Curl()
-        c.setopt(pycurl.COOKIEJAR,  'data/cookie.txt')
-        c.setopt(pycurl.COOKIEFILE, 'data/cookie.txt')
-
-
-        c.setopt(c.URL, "https://planif.esiee.fr:8443/jsp/custom/modules/plannings/direct_planning.jsp?projectId=0"+
-        "&login=" +self.username+
-        "&password="+self.password+
-        "&resources="+rooms)
-        c.perform()
-
-        c.setopt(c.URL, "https://planif.esiee.fr:8443/jsp/custom/modules/plannings/icalDates.jsp")
-        c.perform()
-
-        with open('data/output.ics', 'wb') as f:
-                #c.setopt(c.URL,'https://www.google.fr')
-                c.setopt(c.URL,"https://planif.esiee.fr:8443/jsp/custom/modules/plannings/ical.jsp?"+self.getDateToUrl()+"&ClientCal=palm")
-                c.setopt(c.WRITEDATA,f)
-                c.perform()
-                f.close()
-                c.close()
-
-        self.icsFileName= "data/output.ics";
-
-
-
- 
-        #ton code
-
-
-        return
-
-    def parseICS(self):
-        if(self.online == True and self.areDownload == False):
-            self.getICS()
-
-        icsFile = open(self.icsFileName)
-        self.optdict = icalendar.Calendar.from_ical(icsFile.read())
-            
-        icsFile.close()
-
-        return
-
-    def getDict(self, *lis):
-
-        returnlist = list()
-        #if the file are not parse and load
-        if(self.areParse == False):
-            self.parseICS()
+    import DataCal
+    print(dir(DataCal))
+     
+    dc = DataCal.DataCal.BuilderDataCal(lis)
+    if form.unique_select.get():
+       dc = dc.onlyRoom(form.selected.get())
     
-        
-        for oneRDV in self.optdict.walk():
-            data = dict(oneRDV)
+    dc.render()
 
-        #if no name are pass in list 
-            if not lis:
-                returnlist.append(data)
-
-            else:
-                #the new dict to add 
-                oneElement = dict()
-                for nameOfParam in lis:
-                    if nameOfParam in  data.keys():    
-                        oneElement[nameOfParam] = oneRDV[nameOfParam]
-                    else:
-                        print (nameOfParam +"n'est pas valide ")
-                
-                    
-                returnlist.append(oneElement);
-
-        return returnlist;
-             
-
-lap = DateDataForm()
-lap.online = False 
-dico = lap.getDict('LOCATION','DESCRIPTION','DTSTART','DTEND')
-import agendaEsiee
-bb = agendaEsiee.orderBy(dico)
 ## super filter
+#agendaEsiee.removeRoom(bb,"Vi")
+#agendaEsiee.
+#agendaEsiee.conditionRoom(bb,'[0-9]4[0-9]{2}')
 
-#bb = agendaEsiee.removeRoom(bb,"Vi")
-#bb = agendaEsiee.onlyRoom(bb,"0110")
-bb = agendaEsiee.conditionRoom(bb,'[0-9]4[0-9]{2}')
+#agendaEsiee.render(bb)
 
-print(bb)
-agendaEsiee.render(bb)
-
+if __name__ == "__main__":
+    app = Frame.Frame(None)
+    app.funcToCall = getData
+    app.mainloop()
+ 
